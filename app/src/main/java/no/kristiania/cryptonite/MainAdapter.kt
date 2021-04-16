8c3596e579360409b7b541754c45747cf62b199d
Fredrik Holm Eikrem <fredrik_eikrem@hotmail.com>
2021-04-16 16:12:21 +0200

    addded icons to crypto currency

--- app/src/main/java/no/kristiania/cryptonite/MainAdapter.kt
+++ app/src/main/java/no/kristiania/cryptonite/MainAdapter.kt
@@ -4,7 +4,10 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.crypto_currencies_list.view.*
+import okhttp3.Protocol.Companion.get
+import java.lang.reflect.Array.get
 
 class MainAdapter(val crypto: Crypto): RecyclerView.Adapter<CustomViewHolder>() {
 
@@ -22,6 +25,7 @@ class MainAdapter(val crypto: Crypto): RecyclerView.Adapter<CustomViewHolder>() 
 
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
         val data = crypto.data.get(position)
+
         holder?.view?.textView_crypto_title?.text = data.name
 
         holder?.view?.textView_symbol?.text = data.symbol
@@ -29,6 +33,14 @@ class MainAdapter(val crypto: Crypto): RecyclerView.Adapter<CustomViewHolder>() 
         holder?.view?.textView_crypto_price?.text = data.priceUsd.toString()
 
         holder?.view?.textView_crypto_percentage.text = data.changePercent24Hr
+
+        val icon = holder?.view?.imageView
+
+        val path = "https://static.coincap.io/assets/icons/" + data.symbol?.toLowerCase() + "@2x.png"
+
+        Picasso.get().load(path).into(icon);
+
+
     }
 
 }
